10dbcfbeb04787c3bf0dff764a091b3843965070
The Kicker <cav.lemasters@geneva.edu>
2022-03-24 10:12:27 -0400

    Weather Tools

---
+++ weather/cel-to-far.py
@@ -0,0 +1,5 @@
+c = input("Enter the degrees of Celsius: ")
+
+calc = int(c)*1.8+32
+
+print(str(calc)+"ºF")
---
+++ weather/far-to-cel.py
@@ -0,0 +1,5 @@
+f = input("Enter the degrees of Fahrenheit: ")
+
+calc = (int(f)-32)*5/9
+
+print(str(calc)+"ºC")
