6ce0d6a027265db64367431d5e0e7ba9491dc960
The Kicker <cav.lemasters@geneva.edu>
2022-03-29 14:40:11 -0400

    Squashin :bug:

--- web-tools/youtube-downloader.py
+++ web-tools/youtube-downloader.py
@@ -1,8 +1,5 @@
 from pytube import YouTube
-import pytube, os
-
-from dotenv import dotenv_values
-config = dotenv_values("../.env")
+import os
 
 video_url = input("Please enter the full URL of the video you want to download: ")
 
@@ -13,7 +10,6 @@ def download_Video(yt):
     # print itag, resolution and codec format of Mp4 streams
     print(f"Video itag : {streams.itag} / Resolution : {streams.resolution} / VCodec : {streams.codecs[0]}")
     
-    
   # enter the itag value of resolution on which you want to download the video
   input_itag = input("Select and enter itag value : ")
   # get video using itag vale
@@ -22,7 +18,7 @@ def download_Video(yt):
   title=yt.title
   fn = title.replace(" ", "-")
 
-  fd = os.chdir("/Users/{0}/Desktop/".format(config["user_login"]))
+  os.chdir("Desktop/")
 
   # finally download the YouTube Video...
   video.download(filename=fn+".mp4")
