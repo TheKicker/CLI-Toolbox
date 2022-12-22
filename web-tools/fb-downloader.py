76af838152cdb3f168415b1778ab2c33fd8931af
The Kicker <cav.lemasters@geneva.edu>
2022-12-22 11:56:00 -0500

    Added FB alternate

---
+++ web-tools/fb-downloader.py
@@ -0,0 +1,11 @@
+base = input("Enter the link of the facebook video: ")
+
+base = base.replace("www", "mbasic")
+
+print("\n")
+print("Go to this link: ")
+print(base)
+print("\n")
+print("It will take you to a mobile version of FB.  Right click the video to enter a new tab. ")
+print("Go to the new tab, right click and use 'Save Video As...'")
+print("\n")
